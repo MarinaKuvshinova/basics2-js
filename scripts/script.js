79a2fb386e60330fbb9d72c89ae922758304d51c
MarinaKuvshinova <pn-05@ukr.net>
2021-07-02 01:19:38 +0300

    additionally task

--- scripts/script.js
+++ scripts/script.js
@@ -661,25 +661,32 @@ window.addEventListener('DOMContentLoaded', () => {
             loader = document.createElement('div');
 
         const postData = (body) => {
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-
-                request.addEventListener('readystatechange', () => {
-                    if (request.readyState !== 4) {
-                        return;
-                    }
-    
-                    if (request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json'); //'multipart/form-data'
-    
-                request.send(JSON.stringify(body));
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
             });
+            // return new Promise((resolve, reject) => {
+            //     const request = new XMLHttpRequest();
+
+            //     request.addEventListener('readystatechange', () => {
+            //         if (request.readyState !== 4) {
+            //             return;
+            //         }
+    
+            //         if (request.status === 200) {
+            //             resolve();
+            //         } else {
+            //             reject(request.status);
+            //         }
+            //     });
+            //     request.open('POST', );
+            //     request.setRequestHeader('Content-Type', 'application/json'); //'multipart/form-data'
+    
+            //     request.send(JSON.stringify(body));
+            // });
         };
 
         statusMessage.className = 'loaded';
@@ -701,13 +708,17 @@ window.addEventListener('DOMContentLoaded', () => {
             body[key] = value;
         });
         postData(body)
-            .then(() => {
-                statusMessage.textContent = successMessage;
-                loader.remove();
-                form.reset();
-                setTimeout(() => {
-                    statusMessage.remove();
-                }, 2000);
+            .then((response) => {
+                if (response.status === 200) {
+                    statusMessage.textContent = successMessage;
+                    loader.remove();
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    throw new Error('status network not 200');
+                }
             }).catch(error => {
                 statusMessage.textContent = errorMessage;
                 loader.remove();
